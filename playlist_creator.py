3e4bba8c87bd6d291e0c4bbde9bfc9808107eadd
ALEXANDROS AGRAFIOTIS <44941418+AlexAgrafiotis@users.noreply.github.com>
2020-04-15 02:25:33 +0300

    Update playlist_creator.py

--- playlist_creator.py
+++ playlist_creator.py
@@ -1,7 +1,9 @@
 import os
 import random
 
+
 def Rand(start, end, num): 
+	#This function will generate a list of random integers, choosing between start and end, for a count of num, and return a list.
     res = [] 
   
     for j in range(num): 
@@ -16,7 +18,8 @@ def get_filepaths(directory):
     #tree by walking the tree either top-down or bottom-up. For each 
     #directory in the tree rooted at directory top (including top itself), 
     #it yields a 3-tuple (dirpath, dirnames, filenames).
-    file_paths = []  # List which will store all of the full filepaths.
+   
+	file_paths = []  # List which will store all of the full filepaths.
 
     # Walk the tree.
     for root,directories,files in os.walk(directory):
@@ -31,41 +34,43 @@ def get_filepaths(directory):
 
 #Main Program
 
-music_file_paths = get_filepaths(r"C:\Users\Alex\Desktop\Test\Music") #CHANGE @ PAPRIKA PC
-spots_file_paths = get_filepaths(r"C:\Users\Alex\Desktop\Test\Spots") #CHANGE @ PAPRIKA PC
+music_file_paths = get_filepaths(r"C:\Users\User\Desktop\Test\Music") #Choose your own music folder path
+spots_file_paths = get_filepaths(r"C:\Users\Alex\Desktop\Test\Spots") #Choose your own ad spot folder path
 print(music_file_paths)
 print(spots_file_paths)
-#Create a .txt file with name paprika_list. If it already exists, delete previous and create a new one.
 
 #If .xspf file already exists, delete it.
-path = r"C:\Users\Alex\Desktop" #change @ paprika PC
+path = r"C:\Users\User\Desktop" #Choose where you want your playlist file to be crated
 for filename in os.listdir(path):
-    if(filename == "paprika_list.xspf"):
-        os.remove("paprika_list.xspf")
+    if(filename == "list.xspf"):
+        os.remove("list.xspf")
+	
+#Create a .txt file with name list. If it already exists, delete previous and create a new one.
 
 try:
-  playlist = open("paprika_list.txt", "x")
+  playlist = open("list.txt", "x")
 except: 
-    os.remove("paprika_list.txt")
-    playlist = open("paprika_list.txt", "x")
+    os.remove("list.txt")
+    playlist = open("list.txt", "x")
 
 #This is where the playlist is created.
-#First, we write the header.
+#First, we write the header required for the xspf file format.
 
 playlist.write("""<?xml version="1.0" encoding="UTF-8"?>
 <playlist xmlns="http://xspf.org/ns/0/" xmlns:vlc="http://www.videolan.org/vlc/playlist/ns/0/" version="1">
 	<title>Paprika</title>
 	<trackList>\n""") 
 
+
 random_list = Rand(0,len(music_file_paths)-1,len(music_file_paths)-1)
-song_index = 0
-ad_count = 0   
+song_index = 0 
+ad_count = 0   #Ad index, caps at len(spots_file_path)
 song_count = 0 
-for song in music_file_paths:
+for song in music_file_paths: 
     song_index = random_list[song_count-1]
     playlist.write("<track><location>"+ music_file_paths[song_index]+ "</location></track>\n")
     song_count = song_count + 1
-    if((song_count) % 3 == 0):
+    if((song_count) % 3 == 0): #Change the number 3 to how many music tracks you want between ads.
         if((ad_count + 1) % len(spots_file_paths) == 0):
             playlist.write("<track><location>"+spots_file_paths[ad_count]+"</location></track>\n")
             ad_count = 0
